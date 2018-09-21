a7d4ec7aece34fb743afd87dc2506a46345f650d
Nikolaj Lindberg <nikolaj@stts.se>
2018-09-21 13:23:09 +0200

    server static dir

--- decompserver/static/demo.js
+++ decompserver/static/demo.js
@@ -2,10 +2,18 @@
 
 //let baseURL = window.location.protocol + '//' + window.location.host + window.location.pathname.replace(/\/$/g,"");
 
-let baseURL = window.location;
+let baseURL = window.location.host;
 
 //function init() { 
-    document.getElementById("decomp_button").addEventListener("click", runDecomp);
+document.getElementById("decomp_button").addEventListener("click", runDecomp);
+document.getElementById("input_word")
+    .addEventListener("keyup", function(event) {
+	event.preventDefault();
+	if (event.keyCode === 13) {
+            document.getElementById("decomp_button").click();
+	}
+    });
+
 //}
 
 function runDecomp() {
@@ -14,6 +22,6 @@ function runDecomp() {
     let select = document.getElementById("decomp_select");
     var decomper = select.options[select.selectedIndex].value;
     let decompURL = baseURL + "/decomp/" + decomper + "/" + word;
-
+    
     console.log(decompURL);
 }
