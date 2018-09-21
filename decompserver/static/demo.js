44249e91782ecd8e01e6cda454971050436eb552
nikolaj <nikolaj.lindberg@stts.se>
2018-09-21 18:39:18 +0200

    add/remove pre/suffix

--- decompserver/static/demo.js
+++ decompserver/static/demo.js
@@ -14,6 +14,11 @@ document.getElementById("input_word")
 	}
     });
 
+document.getElementById("add_prefix_button").addEventListener("click", addPrefix);
+document.getElementById("remove_prefix_button").addEventListener("click", removePrefix);
+document.getElementById("add_suffix_button").addEventListener("click", addSuffix);
+document.getElementById("remove_suffix_button").addEventListener("click", removeSuffix);
+
 //}
 
 function runDecomp() {
@@ -39,7 +44,133 @@ function runDecomp() {
 	}
     };
     
+    xhr.send();
+}
+
+
+// TODO Woohoo, copy and paste x 4 below !!!
+
+function addPrefix() {
     
+    let word = document.getElementById("prefix_input").value;
+
+    if (word.trim() === "") {
+	return;
+    }
+    
+    let select = document.getElementById("decomp_select");
+    var decomper = select.options[select.selectedIndex].value;
+    let decompURL = baseURL + "/decomp/" + encodeURIComponent(decomper) + "/add_prefix/" + encodeURIComponent(word);
+    
+    console.log(decompURL);
+    
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', decompURL);
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    
+    xhr.onload = function(evt) {
+	if ( xhr.readyState === 4) {
+     	    if (xhr.status === 200) {
+		let res = xhr.responseText;
+		
+		document.getElementById("message_div").innerText = res;
+	    } // TODO on error
+	}
+    };
+    
+    xhr.send();
+}
+
+function removePrefix() {
+    
+    let word = document.getElementById("prefix_input").value;
+
+    if (word.trim() === "") {
+	return;
+    }
+    
+    let select = document.getElementById("decomp_select");
+    var decomper = select.options[select.selectedIndex].value;
+    let decompURL = baseURL + "/decomp/" + encodeURIComponent(decomper) + "/remove_prefix/" + encodeURIComponent(word);
+    
+    console.log(decompURL);
+    
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', decompURL);
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    
+    xhr.onload = function(evt) {
+	if ( xhr.readyState === 4) {
+     	    if (xhr.status === 200) {
+		let res = xhr.responseText;
+		
+		document.getElementById("message_div").innerText = res;
+	    } // TODO on error
+	}
+    };
+    
+    xhr.send();
+}
+
+
+function addSuffix() {
+    
+    let word = document.getElementById("suffix_input").value;
+
+    if (word.trim() === "") {
+	return;
+    }
+    
+    let select = document.getElementById("decomp_select");
+    var decomper = select.options[select.selectedIndex].value;
+    let decompURL = baseURL + "/decomp/" + encodeURIComponent(decomper) + "/add_suffix/" + encodeURIComponent(word);
+    
+    console.log(decompURL);
+    
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', decompURL);
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    
+    xhr.onload = function(evt) {
+	if ( xhr.readyState === 4) {
+     	    if (xhr.status === 200) {
+		let res = xhr.responseText;
+		
+		document.getElementById("message_div").innerText = res;
+	    } // TODO on error
+	}
+    };
+    
+    xhr.send();
+}
+
+function removeSuffix() {
+    
+    let word = document.getElementById("suffix_input").value;
+
+    if (word.trim() === "") {
+	return;
+    }
+    
+    let select = document.getElementById("decomp_select");
+    var decomper = select.options[select.selectedIndex].value;
+    let decompURL = baseURL + "/decomp/" + encodeURIComponent(decomper) + "/remove_suffix/" + encodeURIComponent(word);
+    
+    console.log(decompURL);
+    
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', decompURL);
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    
+    xhr.onload = function(evt) {
+	if ( xhr.readyState === 4) {
+     	    if (xhr.status === 200) {
+		let res = xhr.responseText;
+		
+		document.getElementById("message_div").innerText = res;
+	    } // TODO on error
+	}
+    };
     
     xhr.send();
 }
